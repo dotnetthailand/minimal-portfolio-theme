d30b4acc7143a90060d51b884e44e25bd3891a1a
aaronamm <theeranitp@gmail.com>
2025-03-30 11:22:06 +0700

    Use react-calendar-heatmap

--- @dotnetthailand/minimal-portfolio-theme/src/components/react/GitHubCalendarWrapper.tsx
+++ @dotnetthailand/minimal-portfolio-theme/src/components/react/GitHubCalendarWrapper.tsx
@@ -1,45 +1,44 @@
-import { useEffect, useRef, useState } from "react";
-import GitHubCalendar from "react-github-calendar";
-import type { Activity } from "react-github-calendar";
 import { subMonths } from 'date-fns';
+// import ActivityCalendar from "react-activity-calendar";
+import CalendarHeatmap, { type ReactCalendarHeatmapValue } from 'react-calendar-heatmap';
+import { match, P } from "ts-pattern";
 
-// import { Tooltip } from 'react-tooltip'
-// import 'react-tooltip/dist/react-tooltip.css'
-/*
-  const renderBlock = (block: BlockElement, activity: Activity) =>
-    cloneElement(block, {
-      'data-tooltip-id': 'react-tooltip',
-      'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
+export interface Activity extends ReactCalendarHeatmapValue<string> {
+  date: string;
+  count: number;
+}
+
+const classForValue = (activity: ReactCalendarHeatmapValue<string> | undefined) => {
+  return match(activity?.count)
+    .with(1, 2, 3, () => `color-github-${activity?.count}`)
+    .with(P.number.gte(4), () => `color-github-4`)
+    .otherwise(() => 'color-empty');
+};
+
+type Props = {
+  activities: Activity[];
+}
+
+
+const activitiesDurationInMonth = 6;
+const endDate = new Date();
+const startDate = subMonths(endDate, activitiesDurationInMonth);
+
+export default function GitHubCalendarWrapper({ activities = [] }: Props) {
+
+  const totalContributions = () => {
+    const activitiesFromStatDate = activities.filter((activity) => {
+      const date = new Date(activity.date);
+      return date.getTime() >= startDate.getTime()
     });
-*/
 
-export default function GitHubCalendarWrapper() {
-  const [isClient, setIsClient] = useState(false);
-  const calendarRef = useRef<HTMLDivElement>(null);
+    const contributionsCount = activitiesFromStatDate.reduce((acc, current) => {
+      acc += current.count;
+      return acc;
+    }, 0);
 
-  const selectLastHalfYear = (activities: Activity[]) => {
-
-    const today = new Date();
-    const fromDate = subMonths(today, 6);
-
-    const filteredAcitities = activities.filter(activity => {
-      const activityDate = new Date(activity.date);
-      return (activityDate.getTime() >= fromDate.getTime());
-    });
-
-    const calendarElement = calendarRef.current!;
-    calendarElement.style.display = 'flex';
-    return filteredAcitities;
-
+    return contributionsCount;
   };
-
-
-  useEffect(() => {
-    const calendarElement = calendarRef.current!;
-    calendarElement.style.display = 'none';
-    setIsClient(true)
-
-  }, []);
 
   // https://github.com/grubersjoe/react-github-calendar/issues/66#issuecomment-2280213874
   // https://github.com/grubersjoe/react-activity-calendar
@@ -49,19 +48,26 @@ export default function GitHubCalendarWrapper() {
   // https://github.com/Bloggify/github-calendar
   // https://grubersjoe.github.io/react-github-calendar/
   // https://github.com/grubersjoe/react-activity-calendar
+  // https://dev.to/andreykh/visualizing-mike-bostocks-github-contributions-calendar-how-to-create-a-js-calendar-chart-3p9m
 
-
-  const labels = { totalCount: '{{count}} contributes in last 6 months' };
-  // calendar use flex direction:column
-  const style = { alignItems: 'start' };
-
+  // https://github.com/k4rthik/git-cal
+  // https://medium.com/@yuichkun/how-to-retrieve-contribution-graph-data-from-the-github-api-dc3a151b4af
+  // https://blog.bitsrc.io/how-to-code-github-style-calendar-heatmap-a197f26c919e
+  // https://blog.bitsrc.io/how-to-code-github-style-calendar-heatmap-a197f26c919e
+  // https://haripo.github.io/react-github-contribution-calendar/
+  // react-github-contribution-calendar
+  // https://github.com/kevinsqi/react-calendar-heatmap
 
   return (
-    <div ref={calendarRef} >
-      {isClient
-        ? <GitHubCalendar style={style} username="aaronamm" colorScheme={'light'} blockSize={8} labels={labels} transformData={selectLastHalfYear} transformTotalCount={false} loading={false} />
-        : <></>
-      }
-    </div>
+    <>
+      <CalendarHeatmap
+        startDate={startDate}
+        endDate={endDate}
+        showWeekdayLabels={true}
+        classForValue={classForValue}
+        values={activities}
+      />
+      <div>{`${totalContributions()} contributes in last ${activitiesDurationInMonth} months`}</div>
+    </>
   )
 }
